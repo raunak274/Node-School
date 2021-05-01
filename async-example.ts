d7969f579649e2619c5a6bb45171e3bbcf5bdbaa
Raunak <Raunak.Kumar@ril.com>
2021-05-01 22:37:45 +0530

    learnyounode commit

---
+++ async-example.ts
@@ -0,0 +1,18 @@
+'use strict'
+
+var fs = require( 'fs');
+var myNumber = undefined
+
+function addOne(callback) {
+fs.readFile('number.txt', function doneReading(err, fileContents) {
+    myNumber = parseInt(fileContents)
+    myNumber++
+    callback()
+    })
+}
+
+function logMyNumber() {
+    console.log(myNumber)
+}
+
+addOne(logMyNumber)
--- my-first-async-io.js
+++ my-first-async-io.js
@@ -1,2 +1,36 @@
 'use strict'
 
+const fs = require( 'fs' );
+
+var number;
+var file;
+
+function addOneNumber(addNumber) {
+    fs.readFile(process.argv[2], function done( err, file) {
+        if(err){
+            console.log(err);
+        }
+        number = file.toString().split('\n').length - 1
+        addNumber()
+    })
+}
+
+addOneNumber(addNumber);
+
+function addNumber() {
+    console.log(number);
+}
+
+
+// 'use strict'
+//     const fs = require('fs')
+//     const file = process.argv[2]
+    
+//     fs.readFile(file, function (err, contents) {
+//       if (err) {
+//         return console.log(err)
+//       }
+//       // fs.readFile(file, 'utf8', callback) can also be used
+//       const lines = contents.toString().split('\n').length - 1
+//       console.log(lines)
+//     })
